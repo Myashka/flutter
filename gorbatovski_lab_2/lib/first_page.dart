27b13269b2230806683504beb00d90f573706279
Alexey Gorbatovskiy <porsh-5@yandex.ru>
2022-03-19 18:07:17 +0300

    Done 2 lab

--- gorbatovski_lab_2/lib/first_page.dart
+++ gorbatovski_lab_2/lib/first_page.dart
@@ -11,7 +11,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageFromState extends State<MyHomePage> {
-  static const String _url = "https://http.cat/";
+  static const String _url = "https://";
 
   final _controller = TextEditingController();
   bool texterror = false;
@@ -35,29 +35,30 @@ class _MyHomePageFromState extends State<MyHomePage> {
             children: <Widget>[
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: const Text('Print some status code',
+                child: const Text('Print some web site',
                     style: TextStyle(fontSize: 20.0, color: Colors.black)),
               ),
               TextFormField(
                 controller: _controller,
                 decoration: InputDecoration(
-                  labelText: 'Status code',
-                  errorText: texterror ? 'Enter correct Status code' : null,
+                  labelText: 'URL',
+                  errorText: texterror
+                      ? 'Enter correct URL with "http://" at the start'
+                      : null,
                 ),
               ),
               const SizedBox(height: 25.0),
               ElevatedButton(
                 onPressed: () {
-                  if (int.tryParse(_controller.text) is! int ||
-                      _controller.text.isEmpty) {
+                  if (_controller.text.isEmpty) {
                     setState(() {
                       texterror = true;
                     });
-                  } else {
+                  } else if (Uri.parse(_controller.text).isAbsolute) {
                     setState(() {
                       texterror = false;
                     });
-                    String url = '$_url' '${_controller.text}';
+                    String url = '${_controller.text}';
                     Navigator.push(
                       context,
                       MaterialPageRoute(
@@ -65,6 +66,10 @@ class _MyHomePageFromState extends State<MyHomePage> {
                       ),
                     );
                     _controller.clear();
+                  } else {
+                    setState(() {
+                      texterror = true;
+                    });
                   }
                 },
                 child: const Text("Let's try!"),
--- gorbatovski_lab_2/lib/second_page.dart
+++ gorbatovski_lab_2/lib/second_page.dart
@@ -3,33 +3,10 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
-Future<CatHTTP> fetchCat(String url) async {
-  final response = await http.get(
-    Uri.parse(url),
-    headers: {"Accept": "application/json", "Access-Control_Allow_Origin": "*"},
-  );
+Future<int> fetchStatusCode(url) async {
+  final response = await http.get(Uri.parse(url));
 
-  if (response.statusCode == 200) {
-    return CatHTTP.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-class CatHTTP {
-  final String src;
-
-  const CatHTTP({
-    required this.src,
-  });
-
-  factory CatHTTP.fromJson(Map<String, dynamic> json) {
-    return CatHTTP(
-      src: json['img']['src'],
-    );
-  }
+  return response.statusCode;
 }
 
 class SecondPage extends StatefulWidget {
@@ -42,47 +19,41 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
-  late Future<CatHTTP> futureCat;
-  String? _url, _body;
-  int? _status;
+  final _formKey = GlobalKey<FormState>();
+  late Future<int> futureStatusCode;
 
   @override
   void initState() {
-    futureCat = fetchCat(widget.url);
-    _sendGet(widget.url);
     super.initState();
-  }
-
-  void _sendGet(url) {
-    http.get(Uri.parse(url)).then((response) {
-      _status = response.statusCode;
-      _body = response.body;
-      print(_body);
-    }).catchError((error) {
-      _status = 0;
-      _body = error.toString();
-      print(_body);
-    });
+    futureStatusCode = fetchStatusCode(widget.url);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.url),
-        ),
-        body: Center(
-          child: FutureBuilder<CatHTTP>(
-            future: futureCat,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Image.network(snapshot.data!.src);
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-              return const CircularProgressIndicator();
-            },
+      key: _formKey,
+      appBar: AppBar(
+        title: Text(widget.url),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            child: FutureBuilder<int>(
+              future: futureStatusCode,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Image.network(
+                      'https://http.cat/' '${snapshot.data!.toString()}');
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return const CircularProgressIndicator();
+              },
+            ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
